d78a6216b39225b4c97692c78c8396b98bf70d94
Waris <kazeewaris05@gmail.com>
2023-02-15 10:44:29 +0000

    CRUD method for state class

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,21 +1,60 @@
 #!/usr/bin/python3
 
 """gettig the state object from databse"""
-import requests
 from models import storage
-from flask import jsonify
+from models.base_model import *
+from flask import abort, jsonify, request, make_response
 from api.v1.views import app_views
 from models.state import State
+
 
 @app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
 def get_states():
     all_state = storage.all('State').values()
     states_list = []
-    print(all_state)
     if request.method == "GET":
-        for state in all_states:
+        for state in all_state:
             states_list.append(state.to_dict())
-        return jsonify(list_states)
+        return jsonify(states_list)
+    elif request.method == "POST":
+        if not request.json:
+            return make_response(jsonify({'error': "Not a JSON"}), 400)
+        elif 'name' not in request.json:
+            return make_response(jsonify({'error': "Missing name"}), 400)
+        else:
+            data = request.json
+            loadData = State(**data)
+            loadData.save()
+            return make_response(loadData.to_dict(), 201)
 
 
+@app_views.route('/states/<state_id>',
+                 methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+def get_states_byID(state_id):
+    """get state by their ID"""
+    state = storage.get(State, state_id)
+    if request.method == "GET":
+        if not state:
+            abort(404)
 
+        return jsonify(state.to_dict())
+    elif request.method == "DELETE":
+        if not state:
+            abort(404)
+        storage.delete(state)
+        storage.save()
+    elif request.method == "PUT":
+        if not state:
+            abort(404)
+        elif not request.json:
+            return make_response(jsonify({'error': "Not a JSON"}), 400)
+
+        else:
+            data = request.get_json()
+            print(data)
+            for key, value in data.items():
+                if key not in ['id', 'created_at', 'updated_at']:
+                    setattr(state, key, value)
+            state.updated_at = datetime.utcnow()
+            storage.save()
+            return make_response(jsonify(state.to_dict()), 200)
