00c0e30584bef0249723f1c69173387e39041057
Waris <kazeewaris05@gmail.com>
2023-02-15 07:16:39 +0000

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,11 +5,13 @@ start a flask app
 """
 
 from api.v1.views import app_views
+from models import storage
+from flask import jsonify
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def Get_status():
     """Get status , getting the status of ap and return json
     """
-    return  jsonify({'status': 'OK'})
+    return  jsonify({"status": "OK"})
 
 
