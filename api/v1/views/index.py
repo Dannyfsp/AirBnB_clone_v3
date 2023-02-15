812344f3ffa727256383525ebbb92bfc7d517320
Dannyfsp <femoscopilala@gmail.com>
2023-02-15 21:52:09 +0000

    fix -indentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 from models import storage
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def Get_status():
     """Get status , getting the status of ap and return json
