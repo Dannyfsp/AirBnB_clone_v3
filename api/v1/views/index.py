e974a72cb946c97f158663712dea1ece3d8b9726
Dannyfsp <femoscopilala@gmail.com>
2023-02-15 21:41:08 +0000

    fix index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,12 +3,7 @@
 """
 start a flask app
 """
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from flask import Flask
 from api.v1.views import app_views
 from models import storage
 from flask import jsonify
