78a8e9baa449c06f7899b26f35094772a10fe847
Waris <kazeewaris05@gmail.com>
2023-02-15 07:58:50 +0000

    stat

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,12 @@
 """
 start a flask app
 """
-
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from api.v1.views import app_views
 from models import storage
 from flask import jsonify
@@ -14,4 +19,17 @@ def Get_status():
     """
     return  jsonify({"status": "OK"})
 
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def Get_stat():
+    """Get stat of each object, 
+    getting the status of ap and return json
+    """
+
+    data = {"users": 'User', "states": 'State', "amenities": 'Amenity',
+             "cities": 'City', "places": 'Place', "reviews": 'Review'}
+    loadData = {}
+    for key, value in data.items():
+        loadData[key] = storage.count(value)
+    return jsonify(loadData)
 
